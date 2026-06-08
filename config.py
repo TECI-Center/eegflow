92166ee9d5be64845356838ad35c78d5b20ef3fc
calvin perumalla <calvinapollos@gmail.com>
2026-06-08 15:26:34 -0700

    conf

---
+++ config.py
@@ -0,0 +1,13 @@
+config = {
+    "fs" : 256,
+    "beta_band" : (12, 30),
+    "gamma_band" : (30, 100),
+    "low_beta_band" : (12, 16),
+    "alpha_band" : (8, 12),
+    "theta_band" : (4, 7),
+    "delta_band" : (0.5, 4),
+    "titles" : [ 'Alpha', 'Beta', 'Gamma', 'Theta', 'Delta'],
+    "colors" : ['dodgerblue','mediumpurple', 'thistle', 'deepskyblue', 'mediumturquoise'],
+    "lowcut" : 0.5,
+    "highcut" : 70
+}
